eeea99296c361acc9c18e41c60322127568ceab6
Abfahim21 <abrarfahim21@gmail.com>
2022-02-10 08:00:14 +0600

    css added

--- react-hooks/src/components/UseStateV2.js
+++ react-hooks/src/components/UseStateV2.js
@@ -1,21 +1,22 @@
-import React, { useState} from 'react'
+import React, {useState} from 'react';
+import './useState.css';
 
 function UseStateV2() {
     const initialValue = 0;
-    const [count, setCount] = useState(initialValue);
+    const [ count, setCount ] = useState(initialValue);
+
     const IncrementTen = () =>{
-        for(let i = 0; i< 10; i++){
-            setCount(prevCount=>prevCount+1)
+        for(let i =0; i<10; i++){
+            setCount(prevCount => prevCount+1)
         }
     }
   return (
     <div>
-        <h3>Count : {count}</h3>
-        <button onClick={ ()=>setCount(prevCount => prevCount+1) }>Increment</button>
-        <button onClick={ ()=>setCount(prevCount => prevCount-1) }>Decrement</button>
-        <button onClick={ ()=>setCount(initialValue) }>Reset</button>
+        <h3>Count: { count }</h3>
+        <button onClick={()=>setCount(prevCount => prevCount+1)}>Increment</button>
+        <button onClick={()=>setCount(prevCount => prevCount-1)}>Decrement</button>
+        <button onClick={()=>setCount(initialValue)}>Reset</button>
         <button onClick={()=>IncrementTen()}>Increment 10</button>
-
     </div>
   )
 }
