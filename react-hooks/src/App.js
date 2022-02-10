f5d6992717f82fbc1a828e56d38de6b6147f7003
Abfahim21 <abrarfahim21@gmail.com>
2022-02-10 07:46:08 +0600

    react useState hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import UseStateV1 from './components/UseStateV1';
+import UseStateV2 from './components/UseStateV2';
 
 function App() {
   return (
     <div className="App">
       <UseStateV1/>
+      <UseStateV2/>
     </div>
   );
 }
