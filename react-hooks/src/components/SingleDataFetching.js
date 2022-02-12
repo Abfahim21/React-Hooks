d1554b8b2adb7e9dbffc1397dfda2a61908e6c84
Abfahim21 <abrarfahim21@gmail.com>
2022-02-12 08:02:48 +0600

    single data fetching by button , axios

--- react-hooks/src/components/SingleDataFetching.js
+++ react-hooks/src/components/SingleDataFetching.js
@@ -4,19 +4,24 @@ import axios from 'axios';
 function SingleDataFetching() {
     const[data, setData] = useState({})
     const[id, setId] = useState(1)
+    const[idFromButtonClick, setIdFromButtonClick] = useState(1)
 useEffect(()=>{
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
     .then((result) => {
         setData(result.data)        
     }).catch((err) => {
         console.log(err)
     });
-},[id])
+},[idFromButtonClick])
+const handleClick = () =>{
+    setIdFromButtonClick(id)
+}
   return (
     <div>
         <h2>Single Data Fetching using Axios</h2>
         <h4>Enter a ID: </h4>
         <input type="text" value={id} onChange={e=>setId(e.target.value)}/>
+        <button type='button' value={id} onClick={handleClick}>Fetch Post Title</button>
         <p>{data.title}</p>
     </div>
   )
